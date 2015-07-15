3383ea4215a1eece4c7987b27b4c15cb5fb573a2
noderosa <chie.miyajima@gmail.com>
2015-07-14 21:53:19 -0400

    update

--- js/app.js
+++ js/app.js
@@ -1,6 +1,50 @@
 
 $(document).ready(function(){
+
+	var computerChoice = Math.random();
 	
+	$('#new').on('click', function (enter) {
+		enter.preventDefault();
+
+		var num = $('#userGuess').val();
+		if (num == "" || isNaN(num)){
+			alert("Please enter a number.");
+		}
+		
+		else if (num % 1 != 0){
+			alert("Please enter a whole number only.");
+		}
+
+		else if (num >= 100 || num <= 0){
+			alert("Please enter a number between 1 and 100.");
+		}
+
+		else {
+
+			newGame();
+		}
+	});
+	
+
+	$('#guessButton').on('click', function (enter) {}
+	var checkTemp = function (difference);
+	
+        if (difference <= 50) {
+            $('#feedback').("ice cold <br>");
+        }
+        else if (difference ) {
+            $('#feedback').("cold <br>");
+        }
+
+        else {
+            $('#feedback').("hot <br>");
+        }
+
+
+  }
+}
+
+
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
     	$(".overlay").fadeIn(1000);
@@ -13,5 +57,6 @@ $(document).ready(function(){
   	});
 
 });
+
 
 
